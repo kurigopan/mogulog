b5ce8baad62b4a6f5cd67723c2aa177faeb2ff69
Negishi <pw.xt.sx@gmail.com>
2025-10-26 23:57:38 +0900

    MITライセンス追加による修正

--- src/components/ui/Copyright.tsx
+++ src/components/ui/Copyright.tsx
@@ -1,7 +1,7 @@
 export function Copyright() {
   return (
     <div className="p-4 text-center">
-      <p className="text-xs text-stone-400">© 2025 もぐログ</p>
+      <p className="text-xs text-stone-400">&copy; 2025 kurigopan</p>
     </div>
   );
 }
